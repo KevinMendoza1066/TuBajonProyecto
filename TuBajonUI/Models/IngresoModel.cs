9d3b5bfd8ca27ff845109eeca29330260c7b1349
isair6786 <124642360+KevinMendoza1066@users.noreply.github.com>
2023-03-11 18:48:31 -0600

    Initial commit

---
+++ TuBajonUI/Models/IngresoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TuBajonUI.Models
+{
+    public class IngresoModel
+    {
+        public string  Nombre { get; set; }
+        public string Contrasenia { get; set; }
+    }
+}
---
+++ TuBajonUI/Models/RegistroModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TuBajonUI.Models
+{
+    public class RegistroModel
+    {
+        public string  Nombre { get; set; }
+        public string Contrasenia { get; set; }
+        public string RepContrasenia { get; set; }
+        public string Correo { get; set; }
+       
+    }
+}
